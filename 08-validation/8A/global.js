311559e48306e9fb6af62f9976c553090fd98f43
Nick <nschette@yahoo.com>
2015-07-07 09:57:12 -0500

    8A questions finished

--- 08-validation/8A/global.js
+++ 08-validation/8A/global.js
@@ -1,5 +1,6 @@
 // 8A
 // Password
+// while loop
 
 var password = "5"
 var input = prompt("What's the password?")
@@ -8,5 +9,13 @@ while (input != password) {
   alert("Incorrect. Guess again.")
   input = prompt("What's the password?")
 }
+
+alert("You got it!")
+
+// for loop
+
+var input1 = prompt("What's the password?")
+
+for (password; input1 != password; input1 = prompt("Incorrect. Guess again."));
 
 alert("You got it!")
